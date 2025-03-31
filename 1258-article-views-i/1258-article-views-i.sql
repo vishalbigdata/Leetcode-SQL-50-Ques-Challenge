327e9bebda6734c748a553116aec1964baa66c42
vishalbigdata <vksrivastava6262@gmail.com>
2025-04-01 03:55:35 +0530

    Time: 851 ms (92.48%), Space: 0B (100%) - LeetHub

--- 1258-article-views-i/1258-article-views-i.sql
+++ 1258-article-views-i/1258-article-views-i.sql
@@ -1,7 +1,5 @@
-# Write your MySQL query statement below
-select distinct( author_id ) as id
-from Views
+/* Write your T-SQL query statement below */
+
+select distinct author_id as id
+from views
 where author_id = viewer_id
-order by author_id asc
-
-
