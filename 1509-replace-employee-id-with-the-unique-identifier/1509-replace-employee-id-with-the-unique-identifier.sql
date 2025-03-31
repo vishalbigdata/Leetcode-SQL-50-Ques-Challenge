837f29a3253773b72dc609725f395117ade27e2a
vishalbigdata <vksrivastava6262@gmail.com>
2025-04-01 03:59:34 +0530

    Time: 1156 ms (89.59%), Space: 0B (100%) - LeetHub

--- 1509-replace-employee-id-with-the-unique-identifier/1509-replace-employee-id-with-the-unique-identifier.sql
+++ 1509-replace-employee-id-with-the-unique-identifier/1509-replace-employee-id-with-the-unique-identifier.sql
@@ -1,5 +1,3 @@
-# Write your MySQL query statement below
-select  eu.unique_id as unique_id , e.name
-from Employees e
-LEFT JOIN EmployeeUNI eu
-    ON e.id = eu.id
+/* Write your T-SQL query statement below */
+select unique_id, name 
+from Employees e left join EmployeeUNI eu on e.id = eu.id
