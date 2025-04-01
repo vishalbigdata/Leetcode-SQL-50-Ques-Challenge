47f935935e6f09af73f92fb18a85ca8c3228bd91
vishalbigdata <vksrivastava6262@gmail.com>
2025-04-01 18:13:14 +0530

    Time: 1482 ms (82.48%), Space: 0B (100%) - LeetHub

--- 1153-product-sales-analysis-i/1153-product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/1153-product-sales-analysis-i.sql
@@ -1,3 +1,4 @@
-/* Write your T-SQL query statement below */
-select product_name, year, price
+# Write your MySQL query statement below
+
+select p.product_name, s.year, s.price
 from Sales s left join Product p on s.product_id = p.product_id
